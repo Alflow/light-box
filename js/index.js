acaa6a6e62725c677317e8814a61ac0d1478c28f
Alfredo Alberruche Herrero <aalberrh@gmail.com>
2024-01-11 13:28:22 +0100

    Added view js function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//const and vars
+
+const images = document.querySelectorAll(`.img`);
+const lightbox = document.querySelector(`.lightbox`);
+const big = document.querySelector(`.big-img`);
+
+//When clicked on .img
+    //add class "isActive" to .lightbox
+    //add src attribute to .big from the .img src attribute
+
+images.forEach((eachImg, index)=>{
+    images[index].addEventListener(`click`,()=>{
+        lightbox.classList.add(`isActive`)
+        big.src = images[index].src
+    })
+})
