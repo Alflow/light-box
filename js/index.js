fda4fce7b55709d53018318d2d5f67422f2cd3a4
Alfredo Alberruche Herrero <aalberrh@gmail.com>
2024-01-11 13:49:30 +0100

    changed click for pointerdown and improved code structure

--- js/index.js
+++ js/index.js
@@ -7,19 +7,27 @@ const lightbox = document.querySelector(`.lightbox`);
 const big = document.querySelector(`.big-img`);
 const closeButton = document.querySelector(`.close`);
 
+
+// functions
+const closeBtnHandler = () => lightbox.classList.remove(`isActive`);
+
+const imagesHandler = (index) =>{
+    lightbox.classList.add(`isActive`);
+    big.src = images[index].src;
+}
+
+
+
+
 //When clicked on .img
     //add class "isActive" to .lightbox
     //add src attribute to .big from the .img src attribute
-
 images.forEach((eachImg, index)=>{
-    images[index].addEventListener(`click`,()=>{
-        lightbox.classList.add(`isActive`)
-        big.src = images[index].src
+    images[index].addEventListener(`pointerdown`,()=>{
+     imagesHandler(index);
     })
 })
 
 
 //When clicked on close X
-closeButton.addEventListener(`click`,()=>{
-    lightbox.classList.remove(`isActive`);
- } );
+closeButton.addEventListener(`click`, closeBtnHandler );
