07acb12386d512f5af806a5fa9b31641270b3c94
Alfredo Alberruche Herrero <aalberrh@gmail.com>
2024-08-13 17:23:08 +0200

    final update!!!

--- js/index.js
+++ js/index.js
@@ -1,11 +1,44 @@
 'use strict';
 
-class TinyLightbox {
+export class TinyLightbox {
   constructor() {
-    this.lightbox = document.querySelector('#tlb-lightbox');
-    this.bigImg = this.lightbox.querySelector('.tlb-lightbox__big-img');
-    this.closeButton = this.lightbox.querySelector('.tlb-lightbox__close');
+    this.lightbox = null;
+    this.bigImg = null;
+    this.closeButton = null;
+    this.createLightbox();
+  }
 
+  createLightbox() {
+    this.lightbox = document.createElement('div');
+    this.lightbox.id = 'tlb-lightbox';
+    this.lightbox.className = 'tlb-lightbox';
+
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'tlb-lightbox__img-container';
+
+    this.closeButton = document.createElement('div');
+    this.closeButton.id = 'tlb-close';
+    this.closeButton.className = 'tlb-lightbox__close';
+    
+    // Aquí deberías insertar el SVG
+    this.closeButton.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    `;
+
+    this.bigImg = document.createElement('img');
+    this.bigImg.className = 'tlb-lightbox__big-img';
+
+    imgContainer.appendChild(this.closeButton);
+    imgContainer.appendChild(this.bigImg);
+    this.lightbox.appendChild(imgContainer);
+    document.body.appendChild(this.lightbox);
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
     this.closeButton.addEventListener('click', () => this.close());
     this.lightbox.addEventListener('click', (event) => {
       if (event.target !== this.bigImg && event.target !== this.closeButton) {
@@ -29,7 +62,3 @@ class TinyLightbox {
     });
   }
 }
-
-// Uso de la librería
-const tlb = new TinyLightbox();
-tlb.attach('.tlb-img');  // Asumiendo que las imágenes tienen la clase .tlb-img
